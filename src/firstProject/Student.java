7e26d1aae779415f9950ef3cd0da930794a9175d
k-burlai <k.burlai@samsung.com>
2025-03-27 17:56:43 +0100

    initial commit

---
+++ src/firstProject/Student.java
@@ -0,0 +1,16 @@
+package firstProject;
+
+public class Student {
+	
+	 public static void main(String[] args) {
+//		System.out.println("asdf");
+		
+		
+		Cookie cookieOne = new Cookie("green");
+		Cookie cookieTwo = new Cookie("yellow");
+		
+		System.out.println(cookieOne.getColor());
+		System.out.println(cookieTwo.getColor());
+
+	 }
+}
